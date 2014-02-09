7e2de9e3e91ec7a40a2da2fbe1315f6c8d55a322
Cristopher López Santana <cristopher.correo@gmail.com>
2014-02-09 22:58:28 +0000

    Symple multiplication evaluated

--- src/Evaluator.py
+++ src/Evaluator.py
@@ -1,8 +1,21 @@
+from src.Number import *
 from src.Operation import *
 
 class Evaluator:
 
-  operation = Operation(0)
+  operation = 0
+  operationSymbols = ['*',':', '+','-']
 
-  if __name__ == "__main__":
-    print(operation.symbol)
+  def getOperation(self, operation):
+    for i in self.operationSymbols:
+      for j in range(0,len(operation)):
+        if i == operation[j]:
+          self.operation = Operation(i, Number(operation[j+1]), Number(operation[j-1]))
+
+
+    return self.operation
+
+if __name__ == "__main__":
+  evaluator = Evaluator
+  result = evaluator.getOperation(evaluator, '1*3')
+  print(result.getResult())
--- src/Operation.py
+++ src/Operation.py
@@ -3,10 +3,15 @@ from src.Result import *
 
 class Operation(Result):
   symbol = ''
+  number1 = 0
+  number2 = 0
 
-  def __init__(self, symbol):
+
+  def __init__(self, symbol, number1, number2):
     self.symbol = symbol
+    self.number1 = number1
+    self.number2 = number2
 
   def getResult(self):
-    raise NotImplementedError("Not implemented")
+    return self.number1.getResult() + self.symbol + self.number2.getResult()
 
